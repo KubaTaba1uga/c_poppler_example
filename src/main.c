7f5b33248679e17f6529a16fbe048e5c12c57698
KubaTaba1uga <luna7337anul@proton.me>
2025-08-12 13:42:30 +0200

    print poppler document title

--- src/main.c
+++ src/main.c
@@ -1,1 +1,26 @@
-int main(void) { return 0; }
+#include "poppler-document.h"
+#include "poppler-structure-element.h"
+#include <poppler.h>
+#include <stdio.h>
+
+int main(void) {
+
+  GFile *file = g_file_new_for_path("src/myinputfile.pdf");
+  if (!file) {
+    puts("Unable to file");
+    return 1;
+  }
+
+  PopplerDocument *pdf =
+      poppler_document_new_from_gfile(file, NULL, NULL, NULL);
+  if (!pdf) {
+    puts("Unable to open pdf");
+    return 1;
+  }
+
+  char *title = poppler_document_get_title(POPPLER_DOCUMENT(pdf));
+
+  printf("Title: %s\n", title);
+
+  return 0;
+}
